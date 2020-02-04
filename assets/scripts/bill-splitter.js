529ecabe2e8d188e2254a287e6d084c402789b07
Jeffersson Tebang <jefftebang0118@gmail.com>
2020-02-04 16:16:37 +0800

    initial commit

---
+++ assets/scripts/bill-splitter.js
@@ -0,0 +1,12 @@
+let split = {
+	bill: 0,
+	friends: 0,
+	share: 0,
+}
+
+// home-page
+let homePage = document.getElementById('home-page');
+
+function bill(){
+	console.log(homePage.firstElementChild.lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild = split.bill);
+}
